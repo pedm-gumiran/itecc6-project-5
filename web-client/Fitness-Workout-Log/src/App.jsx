1a1e1cb7039d05f3de3a53daca92f08603b177e8
pedm-gumiran <gumiranpeter102816@gmail.com>
2025-04-24 10:51:02 +0800

    This is the initial project template

---
+++ web-client/Fitness-Workout-Log/src/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { FaHome } from "react-icons/fa";
+export default function App() {
+  return <div className='font-black text-5xl text-center'>App <FaHome /></div>;
+}
